f0f4853a3df64c5adeb01951406c4c5aa7601b1c
Jitendra Singh <jsroyal55@gmail.com>
2017-10-01 21:49:43 +0530

    Function name changed
    Before merging add a label Hactoberfest

--- largest_sum_contiguous_subarray/largestSumContiguousSubArray.c
+++ largest_sum_contiguous_subarray/largestSumContiguousSubArray.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #define max(a,b) (((a)>(b)) ? (a) : (b))
 
-int largestSumContiguousSubarray(int arr[], int size) {
+int largestSumContiguousSubArray(int arr[], int size) {
 	int max_till = 0;
 	int max_int = 0;
 	for (int i = 0; i < size; i++) {
@@ -16,5 +16,5 @@ int largestSumContiguousSubarray(int arr[], int size) {
 int main() {
 	int array[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4, 5};
 	int size = sizeof(array) / sizeof(array[0]);
-	printf("%d\n", largestSumContiguousSubarray(array, size));
+	printf("%d\n", largestSumContiguousSubArray(array, size));
 }
